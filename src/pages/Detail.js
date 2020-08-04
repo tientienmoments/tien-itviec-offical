91ae0a7039dbcc76ea19c18a398eb30c1f91740a
tientienmoments <tientienmoments@gmail.com>
2020-08-05 00:56:24 +0700

    trial

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,52 +1,67 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Badge,} from "react-bootstrap";
+import { Badge, Button, Row } from "react-bootstrap";
 
 
 
-const apiAddress= process.env.REACT_APP_BACKEND_SERVER_URL
+const apiAddress = process.env.REACT_APP_BACKEND_SERVER_URL
 
-export default function Detail ({jobtitle, props}) {
-    console.log("what is props",props)
-    let {id}= useParams()
-    let [job,setJob]= useState(null)
+export default function Detail({ jobtitle, props }) {
+    console.log("what is props", props)
+    let { id } = useParams()
+    let [job, setJob] = useState(null)
 
-    
+
     let getDetailData = async () => {
-        let url =`${apiAddress}/jobs/${id}`
-        let response = await fetch (url)
+        let url = `${apiAddress}/jobs/${id}`
+        let response = await fetch(url)
         let result = await response.json()
-        console.log("result",result)
+        console.log("result", result)
         setJob(result)
     }
-    
-    useEffect (()=> {
-            
+
+    useEffect(() => {
+
         getDetailData()
 
     }, [])
- 
 
-    if ( job== null) {
-        return<div>loading</div>
+
+    if (job == null) {
+        return <div>loading</div>
     }
 
     return (
         <div>
-            <h1>Detail 
-                <h3>{job.title}</h3>
-                <p>{job.salary}</p>
-                <p>{job.description}</p>
-                <div>
-                {job.tags.map((tag) => (
-                  <Badge variant="secondary">
-                    {tag}
-                  </Badge>
-                ))}
+            <div className="container wrapper d-flex justify-content-center">
+
+
+                <div className="card-w">
+                    <div className="card inset">
+
+                        <div className="card__text">
+                            <h4>{job.title}</h4>
+                            <h5>Salary: {job.salary} $</h5>
+                            <h5>Job's Detail:</h5>
+                            <p>{job.description}</p>
+                            <Row className="apply-style">
+                            <h5>
+                                {job.tags.map((tag) => (
+                                    <Badge variant="secondary" style={{margin:"5px"}}>
+                                        {tag}
+                                    </Badge>
+                                ))}
+                            </h5>
+                            <Button variant="light" style={{height:"50px", width:"120px"}}>APPLY</Button>
+                            </Row>
+
+                        </div>
+                    </div>
                 </div>
-                
-                
-            </h1>
+
+
+
+            </div>
         </div>
     )
 }
--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
 import { useHistory, useLocation } from "react-router-dom";
-import {Form, Row, Col, Button} from 'react-bootstrap'
+import { Form, Row, Col, Button, Badge } from 'react-bootstrap'
 
-
+import Moment from 'react-moment'
 
 
 
@@ -19,9 +19,9 @@ export default function Jobs() {
 
     let query = useQuery();
     let [keyword, setKeyword] = useState(query.get(QUERYSTR_PREFIX));
-     let [jobList, setJobList] = useState([])
+    let [jobList, setJobList] = useState([])
     let history = useHistory()
-    
+
 
 
     const getData = async () => {
@@ -34,7 +34,7 @@ export default function Jobs() {
         } catch (err) {
             console.log("err", err.message)
         }
-        
+
     }
 
 
@@ -77,7 +77,7 @@ export default function Jobs() {
     return (
         <div>
             <div>
-                <Form onSubmit={handleSearch} >
+                <Form onSubmit={handleSearch} style={{ margin: "40px" }} >
                     <Form.Group as={Row}>
                         <Form.Label htmlFor="search" column sm={2}>
                             {" "}
@@ -93,16 +93,59 @@ export default function Jobs() {
                                 onChange={(e) => setKeyword(e.target.value)}
                             ></Form.Control>
                         </Col>
-                        <Button variant="outline-danger" style={{ fontWeight: "bold" }} type="submit">Submit</Button>
+                        <Button variant="outline-danger" style={{ fontWeight: "bold", height: "40px", width: "90px" }} type="submit">Submit</Button>
                     </Form.Group>
                 </Form>
             </div>
 
-            <div>
-                <h1>hehe</h1>
-                {jobList.map((job) => {
-                    return <h3 onClick={() => getDetail(job.id)}>{job.title}</h3>
-                })}
+            <div className="card-style">
+
+                <div className="multi-cards">
+
+                    {jobList.map((job) => {
+                        return <button className="cards-style-detail" onClick={() => getDetail(job.id)}>
+                            <Row style={{ width: "790px", height: "290px", marginLeft: "1px" }}>
+                                <Col sm={3} className="logo-side">
+                                    <img src={job.img} alt="logo" style={{ borderRadius: "50px", marginBottom: "30px" }} />
+                                    <Button variant="light" style={{ fontWeight: "bold", height: "40px", width: "130px", }}>{job.city}</Button>
+                                    <Button variant="light" style={{ fontWeight: "bold", height: "40px", width: "130px" }}>District: {job.district}</Button>
+                                    <Moment fromNow>{job.time}</Moment>
+
+                                </Col>
+                                {/* {job.isHotjob ? (
+                                    <div className="hotjob-label">Hot Job</div>
+                                ) : (
+                                        <div></div>
+                                    )} */}
+
+                                <Col style={{ borderLeft: "0.5px solid rgb(209, 209, 209)" }} sm={9}>
+                                    <h4>{job.title}<span> {job.isHotjob ? (<Badge pill variant="danger">
+                                        Hot
+                                    </Badge>):(<div></div>)}</span></h4>
+
+                                    <hr></hr>
+                                    <div >
+                                        <h4 style={{ textAlign: "right" }}>{job.salary} $</h4>
+                                        <h4 style={{ textAlign: "left" }}>Benefits</h4>
+                                        <ul style={{ textAlign: "left" }}>{job.benefits.map((benefit) => { return <li>{benefit}</li> })}</ul>
+                                        <h5 style={{ textAlign: "left" }}>{job.tags.map((tag) => { return <Badge pill variant="secondary" style={{ margin: "5px" }}>{tag} </Badge> })}</h5>
+
+
+                                    </div>
+
+
+
+                                </Col>
+                            </Row>
+                        </button>
+                    })}
+
+
+                </div>
+
+
+
+
             </div>
 
         </div>
